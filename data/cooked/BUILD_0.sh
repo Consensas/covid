0d922440803da1644d98a0d220002ec28ff18938
David Janes <david@consensas.com>
2020-03-17 16:29:25 -0400

    autobuilding

---
+++ data/cooked/BUILD_0.sh
@@ -0,0 +1,14 @@
+#
+#   BUILD_0.sh
+#
+#   David Janes
+#   Consensas
+#   2020-03-17
+#   ☘️o
+#
+#   Compile all the data
+#
+
+cd $(dirname $0)
+
+node cook.js
---
+++ data/raw/BUILD_2.sh
@@ -0,0 +1,14 @@
+#
+#   BUILD_2.sh
+#
+#   David Janes
+#   Consensas
+#   2020-03-17
+#   ☘️o
+#
+#   Pull raw data
+#
+
+cd $(dirname $0)
+
+node primary.js
