85edbb92152d82f704c769cdc1724ed007dd0038
David Janes <david@consensas.com>
2020-03-18 10:37:14 -0400

    use Covid19Canada

---
+++ data/ca-cases/README.md
@@ -0,0 +1,7 @@
+## Data source
+
+https://docs.google.com/spreadsheets/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/edit#gid=411012049
+
+COVID-19 Canada Open Data Working Group. Epidemiological Data from the COVID-19 Outbreak in Canada. 
+https://github.com/ishaberry/Covid19Canada
+					
---
+++ data/ca-cases/pull.js
@@ -0,0 +1,73 @@
+/*
+ *  data/ca-cases/pull.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2020-03-18
+ *
+ *  Copyright (2013-2020) David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+const google = require("iotdb-google")
+const fs = require("iotdb-fs")
+
+let credentials
+let token
+
+try {
+    credentials = require("../../.cfg/credentials.json")
+    token = require("../../.cfg/token.json")
+} catch (x) {
+    console.log("#", "use bin/google-token to get tokens first")
+}
+
+const googled = {
+    credentials: credentials,
+    token: token,
+}
+
+_.promise({
+    googled: googled,
+})
+    .then(google.initialize)
+    .then(google.auth.token)
+    .then(google.sheets.initialize)
+    .then(google.sheets.list_values.p({
+        spreadsheetId: "1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo",
+        range: "Cases",
+    }))
+    .make(sd => {
+        while (sd.jsons.length && (sd.jsons[0].length < 5)) {
+            sd.jsons.shift()
+        }
+    })
+    .then(google.sheets.headers.first)
+    .each({
+        method: _.promise((sd, sdone) => {
+            _.promise(sd)
+                .add("path", `raw/${sd.json.case_id}.yaml`)
+                .then(fs.make.directory.parent)
+                .then(fs.write.yaml)
+                .end(sdone, sd)
+        }),
+        inputs: "jsons:json"
+    })
+    .make(sd => {
+        console.log("+", sd.jsons.length)
+    })
+    .catch(_.error.log)
