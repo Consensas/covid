76969223383825dc171b18500a81d55ed0195939
David Janes <david@consensas.com>
2020-03-20 08:26:41 -0400

    add build function

---
+++ data/ca-qc.cmo/BUILD_1.sh
@@ -0,0 +1,17 @@
+#
+#   BUILD_1.sh
+#
+#   David Janes
+#   Consensas
+#   2020-03-20
+#
+#   Update local database
+#
+
+set -e
+cd $(dirname $0)
+
+node pull.js
+node cook.js
+git add raw/*.yaml
+git commit -am "new raw data"
