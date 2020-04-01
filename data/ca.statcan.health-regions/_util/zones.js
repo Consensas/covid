fd81ed07f0864b07e0dfc8fb22546a6ef277c6c5
David Janes <david@consensas.com>
2020-04-01 12:18:06 -0400

    updated list of zones

---
+++ data/ca.statcan.health-regions/_util/zones.js
@@ -0,0 +1,122 @@
+const _ = require("iotdb-helpers")
+const fs = require("iotdb-fs")
+const document = require("iotdb-document")
+
+_.promise()
+    .then(fs.read.yaml.p("zones.yaml"))
+    .make(sd => {
+        const zones = []
+
+        sd.json.forEach(region => {
+            if (region.zones.length <= 1) {
+                region.zones.fragments = region.zones.fragments || []
+                return
+            }
+
+            region.zones.forEach(zone => {
+                if (!zone.fragments) {
+                    let fragment 
+                    switch (region.region) {
+                    case "NS":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/\s*-\s*/, ",")
+                            .replace(/^zone/, "")
+                        break
+
+                    case "NB":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/[)]$/, "")
+                            .replace(/ [(]/, ",")
+                            .replace(/ area/, "")
+                            .replace(/^zone/, "")
+                        break
+
+                    case "NL":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/ regional integrated health authority$/, "")
+                        break
+
+                    case "QC":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/région du /, "")
+                            .replace(/région de l'/, "")
+                            .replace(/région de la /, "")
+                            .replace(/région des /, "")
+                            .replace(/région de /, "")
+                        break
+
+                    case "BC":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/ health service delivery area$/, "")
+                        break
+
+                    case "ON":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/ district health unit$/, "")
+                            .replace(/ health unit$/, "")
+                            .replace(/ regional$/, "")
+                            .replace(/^the /, "")
+                            .replace(/^city of /, "")
+                            .replace(/ and$/, "")
+                        break
+
+                    case "AB":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/ zone$/, "")
+                        break
+
+                    case "SK":
+                        fragment = zone.name
+                            .toLowerCase()
+                            .replace(/ regional health authority$/, "")
+                            .replace(/ health authority$/, "")
+                            .replace(/ health authorities$/, "")
+                        break
+                    }
+
+                    if (fragment) {
+                        zone.fragments = fragment.split(/(,|\band)/)
+                            .map(x => x.trim())
+                            .filter(x => x !== "and")
+                            .filter(x => x !== ",")
+                            .filter(x => x.length)
+                    }
+                }
+
+                zone = {
+                    "@id": zone["@id"],
+                    country: "CA",
+                    region: region.region,
+                    identifier: `${zone.id}`,
+                    ...zone,
+                }
+                delete zone.id
+
+                zones.push(zone)
+
+                /*
+                if (region.region == "SK") {
+                    console.log(zone)
+                }
+                */
+            })
+        })
+
+        sd.json = zones
+        
+    })
+    .then(document.from.yaml)
+    .make(sd => {
+        sd.document = sd.document
+            .replace(/^-/mg, "\n-")
+            .replace(/^    /mg, "  ")
+    })
+    .then(fs.write.stdout)
+    .catch(_.error.log)
